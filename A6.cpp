f47c9a38c6efebc95c8282f941d062ff7785e76d
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2008-10-02 19:52:15 +0200

    A6 - ANS

---
+++ A6.cpp
@@ -0,0 +1,52 @@
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+double x,y,c_;
+
+inline double f(double w)
+{
+	return ((1.0/sqrt((y*y)-(w*w)))+(1.0/sqrt((x*x)-(w*w))))-1/c_;
+}
+
+inline double min(double l,double p)
+{
+	if(l<p)
+		return l;
+	else
+		return p;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	unsigned int z;
+	cin>>z;
+	while(z--)
+	{
+	double a,b,c,d, pr=0.0001;
+	cin>>x>>y>>c_;
+	a=x;b=y;
+	c=min(a,b)/2;
+	a-=0.00001;b-=0.00001;
+	do
+	{
+		//cout<<"a = "<<a<<" b = "<<b<<" r��nica: "<<abs(b-a)<<" c = ";
+		//cout<<c<<" f(a) = "<<f(a)<<" f(c) = "<<f(c)<<endl;
+		if (f(a)*f(c)<0)
+			b = c;
+		else
+			a = c;
+		//cout<<"a = "<<a<<" b = "<<b<<endl;
+		c = (a+b)/2;
+	} while (abs(b-a) > pr);
+	cout<<fixed<<setprecision(3)<<c<<'\n';
+// 	while(true)
+// 	{
+// 		cin>>d;
+// 		cout<<f(d)<<endl;
+// 	}
+	}
+	return 0;
+}
