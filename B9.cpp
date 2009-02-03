4c8c40af067d38ff023e113037517d8ca03aebc9
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2009-02-03 15:52:53 +0100

    B9 - ANS

---
+++ B9.cpp
@@ -0,0 +1,93 @@
+#include <cstdio>
+#include <string>
+#include <map>
+
+using namespace std;
+
+uint32_t	D[200][200];
+string		*N;
+char		temp[11];
+char		temp2[11];
+uint32_t	n;
+
+const uint32_t inf=0xffffffff-1000002;
+
+int main()
+{
+	uint32_t	z;
+	scanf("%u",&z);
+	while(z--)
+	{
+		map<string,uint32_t>	M;
+		uint32_t				m;
+		scanf("%u\n",&n);
+		
+		N=new string[n];
+		
+		for(uint32_t i=0;i<n;++i)
+			for(uint32_t j=0;j<n;++j)
+				D[i][j]=inf;
+			
+		for(uint32_t i=0;i<n;++i)
+			D[i][i]=0;
+		
+		for(uint32_t i=0;i<n;++i)
+		{
+			gets(temp);
+			N[i]=temp;
+			M.insert(pair<string,uint32_t>(temp,i));
+		};
+		
+		/*for(map<string,uint32_t>::iterator i=M.begin(),e=M.end();i!=e;++i)
+		{
+			printf("%s %u\n",i->first.c_str(),i->second);
+		}
+		
+		for(uint32_t i=0;i<n;++i)
+			printf("%s\n",N[i].c_str());*/
+			
+		scanf("%u\n",&m);
+		while(m--)
+		{
+			scanf("%s %s",temp,temp2);
+			scanf("%u\n",&D[M[temp]][M[temp2]]);
+			D[M[temp2]][M[temp]]=D[M[temp]][M[temp2]];
+		}
+		
+		/*for(uint32_t i=0;i<n;++i)
+		{
+			for(uint32_t j=0;j<n;++j)
+				printf("%u\t\t",D[i][j]);
+			putchar('\n');
+		}*/
+		
+		for(uint32_t i=0;i<n;++i)
+			for(uint32_t j=0;j<n;++j)
+				for(uint32_t k=0;k<n;++k)
+					if(D[j][k]>D[j][i]+D[i][k])
+						D[j][k]=D[j][i]+D[i][k];
+		
+		/*for(uint32_t i=0;i<n;++i)
+		{
+			for(uint32_t j=0;j<n;++j)
+				printf("%u\t\t",D[i][j]);
+			putchar('\n');
+		}*/
+		
+		printf("%12s","");
+		for(uint32_t i=0;i<n;++i)
+			printf("%12s",N[i].c_str());
+		putchar('\n');
+		
+		for(uint32_t i=0;i<n;++i)
+		{
+			printf("%12s",N[i].c_str());
+			for(uint32_t j=0;j<n;++j)
+				printf("%12u",D[i][j]);
+			putchar('\n');
+		}
+		
+		delete [] N;
+	}
+	return 0;
+}
