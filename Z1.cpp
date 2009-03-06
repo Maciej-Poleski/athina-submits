248cd664813be489ab8bcbdb00ab546cf82dc9fa
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2009-03-06 21:28:09 +0100

    Z1 - CME

--- Z1.cpp
+++ Z1.cpp
@@ -1,20 +1,38 @@
-#include <stdio.h>
+#include <cstdio>
+#include <list>
+#include <slist>
 
-unsigned int n;
+using namespace std;
+
+list<int>   l1,l2;
+slist<int>  t;
 
 int main()
 {
-	scanf("%d",&n);
-	if(n==0)
-	{
-		puts("1");
-		return 0;
-	}
-	unsigned int i=n;
-	while(--i)
-		n*=i;
-	printf("%d\n",n);
-
-	return 0;
+    l1.push_back(1);
+    l1.push_back(2);
+    l1.push_back(3);
+    l2.push_back(5);
+    l2.push_back(6);
+    for(list<int>::iterator i=l1.begin(),e=l1.end();i!=e;++i)
+    {
+        printf("%d ",*i);
+    }
+    putchar('\n');
+    for(list<int>::iterator i=l2.begin(),e=l2.end();i!=e;++i)
+    {
+        printf("%d ",*i);
+    }
+    putchar('\n');
+    l1.splice(l1.end(),l2);
+    for(list<int>::iterator i=l1.begin(),e=l1.end();i!=e;++i)
+    {
+        printf("%d ",*i);
+    }
+    putchar('\n');
+    for(list<int>::iterator i=l2.begin(),e=l2.end();i!=e;++i)
+    {
+        printf("%d ",*i);
+    }
+    putchar('\n');
 }
-
