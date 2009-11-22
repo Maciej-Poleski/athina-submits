bde810178b319369517707c6c419da3a5d8c195e
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2009-11-22 18:47:34 +0100

    D5 - TLE

--- D5.cpp
+++ D5.cpp
@@ -5,10 +5,11 @@
 /* Obsługiwana jest tylko architektura o kolejności bajtów Little-endian */
 /* Wczesne wiązanie */
 #define pow2(i)     ((1)<<(i))
-#define get(x,i)    (((x)>>(i))&(1))
-#define set(x,i)    ((x)=((x)|(pow2(i))))
-#define clear(x,i)  ((CHOKE ME!))             // Tego nie potrzebujemy!
+#define get(x,i)    (((x)>>((i)-(1)))&(1))
+#define set(x,i)    ((x)=((x)|(pow2((i)-(1)))))
+#define clear(x,i)  ((x)=((x)&(~(pow2((i)-(1))))))
 
+#ifdef ENABLE_OBSOLETE_CODE
 int      n;
 uint32_t T[20];                               // Wystarczy
 
@@ -28,6 +29,20 @@ uint64_t compute(uint8_t k)
 	}
     return wynik;
 }
+#else
+int        n;
+uint64_t   T[1<<21];
+bool       A[21][21];
+
+int bitcount(uint32_t x)
+{
+    int w=0;
+    for(int i=1;i<=30;++i)
+	w+=get(x,i);
+    return w;
+}
+
+#endif
 
 int main()
 {
@@ -36,6 +51,7 @@ int main()
     while(z--)
     {
 	scanf("%d\n",&n);
+#ifdef USE_OBSOLETE_CODE
 	memset(T,0,n*4);
 	for(int i=0;i<n;++i)
 	{
@@ -46,6 +62,29 @@ int main()
 	    getchar();
 	}
 	printf("%llu\n",compute(static_cast<uint8_t>(0)));
+#else
+	for(int i=1;i<=n;++i)
+	{
+	    for(int j=1;j<=n;++j)
+	    {
+		A[i][j]=((getchar()=='.')?true:false);
+	    }
+	    getchar();
+	}
+	T[0]=1;
+	for(uint32_t S=1;S<=(1<<n);++S)
+	{
+	    T[S]=0;
+	    for(int k=1;k<=25;++k)
+	    {
+		if(get(S,k))
+		{
+		    T[S]+=A[bitcount(S)][k]*T[S&(~(1<<(k-1)))];
+		}
+	    }
+	}
+	printf("%llu\n",T[(1<<n)-1]);
+#endif
     }
     return 0;
 }
