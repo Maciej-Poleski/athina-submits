5b7a1940d097532b4fa73b2d7d56c10620c296d5
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2010-02-18 18:34:35 +0100

    T1 - RTE

--- T1.cpp
+++ T1.cpp
@@ -1,4 +1,28 @@
 #include <cstdio>
+#include <cstring>
+
+const int maxSize=2000000;
+
+char W[maxSize];
+char T[maxSize];
+int n;
+int PS[maxSize+1];
+int R[maxSize+1];
+int A[maxSize+1];
+int B[maxSize+1];
+
+void kmp(char* const W,const int N,int* const PS,char* const T,const int M,int* const R)
+{
+    int t=-1;
+    for(int j=0;j<=M;++j)
+    {
+	while(t>=0 && T[j-1]!=W[t])
+	    t=PS[t];
+	R[j]=++t;
+	if(t==N)
+	    t=PS[t];
+    }
+}
 
 int main()
 {
@@ -6,44 +30,33 @@ int main()
     scanf("%d\n",&z);
     while(z--)
     {
-        int n;
-        scanf("%d\n",&n);
-        char text[n];
-        scanf("%s\n",text);
+	scanf("%s\n",W);
+	n=strlen(W);			// Długość W - n
+	for(int i=n-1;i>=0;--i)		// T=W.reverse()
+	    T[n-i-1]=W[i];
+	T[n]=0;
 
-        int MPNext[n+1],b=-1,w=0;
-        MPNext[0]=b;
+	PS[0]=R[0]=-1;
+	kmp(W,n,PS,W+1,n-1,PS+1);
+	kmp(T,n,R,T+1,n-1,R+1);
 
-        /*for(int i=1;i<=n;++i)
-        {
-            while((b>-1) && text[i-1]!=text[b])
-                b=KMPNext[b];
-            ++b;
-            if(text[i]!=text[b] || b==n)
-            {
-                KMPNext[i]=b;
-                ++w;
-            }
-            else
-                KMPNext[i]=KMPNext[b];
-        }*/
+	kmp(T,n,R,W,n,A);
+	kmp(W,n,PS,T,n,B);
 
-        for(int i=0;i<n;++i)
-        {
-            while((b>-1) && text[i]!=text[b])
-                b=MPNext[b];
-            MPNext[i+1]=++b;
-        }
-
-        for(int i=1;i<=n;++i)
-            if(i%(i-MPNext[i])==0 && i/(i-MPNext[i])>1)
-                ++w;
-
-        printf("%d\n",w);
-
-        for(int i=1;i<=n;++i)
-            if(i%(i-MPNext[i])==0 && i/(i-MPNext[i])>1)
-                printf("%d %d\n",i,i/(i-MPNext[i]));
+	if(A[n]>B[n])
+	{
+	    printf("%s",W);
+	    for(int i=A[n];i<n;++i)
+		putchar(T[i]);
+	    printf("\n");
+	}
+	else
+	{
+	    printf("%s",T);
+	    for(int i=B[n];i<n;++i)
+		printf("%c",W[i]);
+	    printf("\n");
+	}
     }
     return 0;
 }
